a6a14f1deade991f28c382e52d99890f58c05f48
AnteUjcic <58471899+AnteUjcic@users.noreply.github.com>
2021-02-01 08:34:18 +0100

    Add files via upload

---
+++ MonthCalendarControla/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MonthCalendarControla
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TimeSpan ts = monthCalendar1.SelectionEnd - monthCalendar1.SelectionStart;
+
+            if (ts.Days < 1)
+
+            {
+
+                MessageBox.Show("Korištenjem tipke SHIFT odaberite opseg datuma.");
+
+                return;
+
+
+            }
+
+            MessageBox.Show("Rezervirali ste usluge u trajanju od "+ts.Days.ToString() + "dana.", "Rezervacija");
+
+
+            MessageBox.Show("Vasa rezervacija počinje " + monthCalendar1.SelectionStart.ToShortDateString() + " a završava se" + monthCalendar1.SelectionEnd.ToShortDateString());
+
+        }
+    }
+}
